bf2b64154e4cb9a1ad44e9a9dccc3e264cd99451
teoks0199 <teokaisheng3@gmail.com>
2023-10-01 18:22:46 +0800

    no message

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ def run_python_file(file_path):
     except Exception as e:
         st.error(f"Error executing '{file_path}': {e}")
 
-script_path = "interactiveplot.py"
-current_permissions = os.stat(script_path).st_mode
-if not current_permissions & 0o111:
-    os.chmod(script_path, current_permissions | 0o111)
-    st.text("Execute permissions added.")
-st.text(f"Current permissions: {current_permissions:o}")
+# script_path = "interactiveplot.py"
+# current_permissions = os.stat(script_path).st_mode
+# if not current_permissions & 0o111:
+#     os.chmod(script_path, current_permissions | 0o111)
+#     st.text("Execute permissions added.")
+#st.text(f"Current permissions: {current_permissions:o}")
 
 st.set_page_config(page_title="EZPack", page_icon=":smiley:")
 
